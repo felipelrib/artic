a2c199fc32f0ccc0c6f51c47e67cb5f61c24f5ec
Diego Barros <diegobsilveira@gmail.com>
2021-11-22 16:22:02 -0300

    Adicionar informações básicas das Arts de um usuário

--- app/pages/[username]/portfolio.js
+++ app/pages/[username]/portfolio.js
@@ -19,7 +19,6 @@ import {
 
 // TODO: Add user data to context
 async function fetchUser(username) {
-	console.log({ apiUrl: process.env.STRAPI_API_URL });
 	const response = await fetch(process.env.STRAPI_API_URL + `/artic-users/?Username=${username}`);
 	if (response.ok) {
 		let users = await response.json();
@@ -61,37 +60,36 @@ function BasicArtistInfo({ user }) {
 	);
 }
 
-function WorkCard({ work }) {
+function ArtCard({ art }) {
 	const theme = useMantineTheme();
 
 	const secondaryColor =
 		theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7];
 
+	const photo = art.media ? `${process.env.STRAPI_API_URL}${art.media.url}` : undefined;
+
 	return (
 		<Card shadow='md' withBorder>
 			<Card.Section>
-				<Image
-					src='https://images.unsplash.com/photo-1634698046002-73019c646268?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=875&q=80'
-					height={160}
-					alt='Imagem descritiva do projeto'
-				/>
+				<Image src={photo} height={200} alt='Imagem descritiva do projeto' />
 			</Card.Section>
 
 			<Group position='apart' style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
-				<Text weight={500}>Nome da obra</Text>
-				<Badge color='pink' variant='light'>
-					Tag 1
-				</Badge>
-				<Badge color='pink' variant='light'>
-					Tag 2
-				</Badge>
+				<Text weight={500}>{art.name}</Text>
+				<Group position='right' style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
+					{art.tags?.map((tag) => (
+						<Badge color='pink' variant='light'>
+							{tag.name}
+						</Badge>
+					))}
+				</Group>
 			</Group>
 
-			<Text size='sm' style={{ color: secondaryColor, lineHeight: 1.5 }}>
-				Nostrum corporis sit consequatur laudantium id. Ullam error tempore voluptatum natus
-				aspernatur sit. Odio illum beatae necessitatibus numquam eos. Et consequatur eaque
-				dolor. Ipsa aspernatur eveniet eveniet. Veritatis sap...
-			</Text>
+			{art.description && (
+				<Text size='sm' style={{ color: secondaryColor, lineHeight: 1.5 }}>
+					{art.description}
+				</Text>
+			)}
 
 			<Button variant='light' color='blue' fullWidth style={{ marginTop: 14 }}>
 				Ver mais
@@ -104,7 +102,7 @@ export default function WorksOfUser({ user }) {
 	const router = useRouter();
 	const { username } = router.query;
 
-	let works = [1, 2, 3, 4, 5, 6]; // let { works } = user;
+	const { arts } = user;
 
 	return user ? (
 		<>
@@ -112,16 +110,16 @@ export default function WorksOfUser({ user }) {
 				<title>{username} | Obras</title>
 			</Head>
 			<BasicArtistInfo user={user} />
-			{works && (
+			{arts && arts.length > 0 && (
 				<>
 					<Space h='xl' />
 					<Divider size='sm' />
 					<Space h='xl' />
 					<Space h='sm' />
 					<SimpleGrid cols={3} spacing='lg'>
-						{works.map((work, idx) => (
-							<Container key={idx} m='sm' size='xs'>
-								<WorkCard work={work} />
+						{arts.map((art) => (
+							<Container key={art.id} m='sm' size='xs'>
+								<ArtCard art={art} />
 							</Container>
 						))}
 					</SimpleGrid>
