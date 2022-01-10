eeec87d44e9ea77fc5b5d10c59a0e0e402d58b32
Rafael Pardini <rafael.pardini99@gmail.com>
2022-01-09 22:25:14 -0300

    Exporta componente de grid de Artes

--- app/pages/[username]/index.js
+++ app/pages/[username]/index.js
@@ -3,16 +3,14 @@ import { useRouter } from 'next/router';
 import Head from 'next/head';
 
 import {
-	Container,
 	Divider,
 	Space,
-	SimpleGrid,
-	Text,
+	Text
 } from '@mantine/core';
 
 import ArtistInfo from '../components/ArtistInfo';
 
-import ArtCard from '../components/ArtCard';
+import ArtsGrid from '../components/ArtsGrid';
 
 // TODO: Add user data to context
 async function getUserByUsername(username) {
@@ -33,7 +31,7 @@ export async function getServerSideProps({ params }) {
 	};
 }
 
-export default function WorksOfUser({ user }) {
+export default function Index({ user }) {
 	const router = useRouter();
 	const { username } = router.query;
 
@@ -45,27 +43,11 @@ export default function WorksOfUser({ user }) {
 				<title>{username} | Obras</title>
 			</Head>
 			<ArtistInfo user={user} />
-			{arts && arts.length > 0 && (
-				<>
-					<Space h='xl' />
-					<Divider size='sm' />
-					<Space h='xl' />
-					<Space h='sm' />
-					<SimpleGrid
-						cols={3}
-						spacing='lg'
-						breakpoints={[
-							{ maxWidth: 'md', cols: 2, spacing: 'md' },
-							{ maxWidth: 'sm', cols: 1, spacing: 'sm' },
-						]}>
-						{arts.map((art) => (
-							<Container key={art.id} m='sm' size='xs'>
-								<ArtCard art={art} />
-							</Container>
-						))}
-					</SimpleGrid>
-				</>
-			)}
+			<Space h='xl' />
+			<Divider size='sm' />
+			<Space h='xl' />
+			<Space h='sm' />
+			<ArtsGrid arts={arts} />
 		</>
 	) : (
 		<>
