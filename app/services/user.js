a8072673b226845bfb004597c351d9d0e654124e
Felipe Ribeiro <felipelrib@gmail.com>
2022-01-10 12:22:55 -0300

    Add test environment for Strapi with empty db and create tests for userService using this environment

--- app/services/user.js
+++ app/services/user.js
@@ -3,10 +3,24 @@ import api from './api.js';
 const getUser = async (username) => {
     let user = null;
     const response = await api.get(`/artic-users?Username=${username}`);
+    if (Array.isArray(response.data) && response.data.length > 0) {
+        user = response.data[0];
+    }
+    return user;
+};
+
+const createUser = async (params) => {
+    let user = null;
+    const response = await api.post(`/artic-users`, params);
     if (response.data) {
         user = response.data;
     }
     return user;
+};
+
+const deleteUser = async (user) => {
+    const response = await api.delete(`/artic-users/${user.id}`);
+    return true;
 }
 
-export { getUser };
+export { getUser, createUser, deleteUser };
--- app/services/user.test.js
+++ app/services/user.test.js
@@ -1,8 +1,43 @@
-const { getUser } = require('./user.js');
+const { getUser, createUser, deleteUser } = require('./user.js');
 
 describe("userService", () => {
-    it("should get a user by username", async () => {
-        const user = await getUser("felipelrib");
-        expect(user).not.toBe(null)
+    it("should create an user", async () => {
+        const params = {
+            Username: "createUser"
+        }
+
+        let user = await getUser(params.Username);
+        expect(user).toBe(null);
+
+        await createUser(params);
+
+        user = await getUser(params.Username);
+        expect(user).not.toBe(null);
+        await deleteUser(user);
+    });
+
+    it("should keep the params after creating", async () => {
+        const params = {
+            Username: "createUserWithParams",
+            Bio: "Test"
+        }
+        await createUser(params);
+
+        const user = await getUser(params.Username);
+        expect(user.Bio).toBe(params.Bio);
+
+        await deleteUser(user);
+    });
+
+    it("should delete an user", async () => {
+        const params = {
+            Username: "deleteUser"
+        }
+
+        let user = await createUser(params);
+        await deleteUser(user);
+
+        user = await getUser(params.Username);
+        expect(user).toBe(null);
     });
 });
