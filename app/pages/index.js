87481ccad48904ef3c7678ed453f492d36dc9cdd
Diego Barros <diegobsilveira@gmail.com>
2022-02-13 14:41:27 -0300

    Add cypress and config for integration/e2e testing, and initial tests for homepage

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,31 +1,47 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { Container, Center, Title, Text, Group } from '@mantine/core'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { Container, Center, Title, Text, Group } from '@mantine/core';
 import { Button } from '@mantine/core';
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Artic</title>
-        <meta name="description" content="App for comissioning artists" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      
-      <Container className={styles.main} fluid>
-        <Container>
-          <Center>
-            <Group position="center" direction="column">
-              <Title order={1}>Artic</Title>
-              <Text align="center">Making life easier for art, and art easier for life.</Text>
-              <Group position="center" direction="row">
-                <Button variant="gradient" gradient={{ from: 'teal', to: 'lime', deg: 105 }} size="lg">Search for art</Button>
-                <Button variant="gradient" gradient={{ from: 'teal', to: 'lime', deg: -105 }} size="lg">Publish yours</Button>
-              </Group>
-            </Group>
-          </Center>
-        </Container>
-      </Container>
-    </div>
-  )
+	return (
+		<div>
+			<Head>
+				<title>Artic</title>
+				<meta name='description' content='App for comissioning artists' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<Container className={styles.main} fluid>
+				<Container>
+					<Center>
+						<Group position='center' direction='column'>
+							<Title className='home-name' order={1}>
+								Artic
+							</Title>
+							<Text className='home-description' align='center'>
+								Making life easier for art, and art easier for life.
+							</Text>
+							<Group className='home-actions' position='center' direction='row'>
+								<Button
+									variant='gradient'
+									gradient={{ from: 'teal', to: 'lime', deg: 105 }}
+									size='lg'
+								>
+									Search for art
+								</Button>
+								<Button
+									variant='gradient'
+									gradient={{ from: 'teal', to: 'lime', deg: -105 }}
+									size='lg'
+								>
+									Publish yours
+								</Button>
+							</Group>
+						</Group>
+					</Center>
+				</Container>
+			</Container>
+		</div>
+	);
 }
